97f5e7a4c8478c443d3746d7df429295581e9a94
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-16 16:17:51 +0500

    ali

--- routes/api.php
+++ routes/api.php
@@ -18,15 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // routes/api.php
-
+Route::any('doctorall', [DoctorController::class, 'doctorAll']);
+Route::any('doctorspeciality', [DoctorController::class, 'doctorSpeciality']);
+Route::any('doctor/get', [DoctorController::class, 'getdoctor']);
+Route::any('doctor/detail', [DoctorController::class, 'doctorShow']);
+Route::any('doctorlogin', [AuthController::class, 'doctorLogin']);
 Route::any('register', [AuthController::class, 'patientRegister']);
 Route::any('patient/login', [AuthController::class, 'patientLogin']);
 Route::group(['middleware' => 'auth:doctor_api'], function () {
-    Route::any('doctorall', [DoctorController::class, 'doctorAll']);
-    Route::any('doctorspeciality', [DoctorController::class, 'doctorSpeciality']);
-    Route::any('doctor/get', [DoctorController::class, 'getdoctor']);
-    Route::any('doctor/detail', [DoctorController::class, 'doctorShow']);
-    Route::any('doctorlogin', [AuthController::class, 'doctorLogin']);
+ 
 });
 Route::group(['middleware' => 'auth:web'], function () {
 });
